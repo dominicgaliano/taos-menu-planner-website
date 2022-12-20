5bbc464236bc125f7b9a3526bb01770050d75707
Dominic Galiano <dgaliano@utexas.edu>
2022-12-19 20:50:12 -0600

    added fake post data

---
+++ client/src/assets/postData.js
@@ -0,0 +1,4 @@
+// TODO: make fake post data
+export default data = {
+  test: 1,
+};
---
+++ client/src/components/Post.tsx
@@ -0,0 +1,5 @@
+function Post() {
+  return <div className="container bg-gray-200">Post</div>;
+}
+
+export default Post;
